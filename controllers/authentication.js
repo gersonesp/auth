d81fa9eb2e555362503b537d17c2fcd20448e75e
Gerson Espinal <gerson.espinal@gmail.com>
2019-09-06 14:28:54 -0400

    Add bcrypt for hashing passwords

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,6 +4,12 @@ exports.signup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "You must provide email and password" });
+  }
+
   User.findOne({ email: email }, (error, existingUser) => {
     if (error) {
       return next(error);
